bb71d1b7d231d4122f35b3cfa8f936d47e33e0fc
Igor <88189374+igor399@users.noreply.github.com>
2022-09-06 16:28:21 +0300

    init & destroy methods

--- src/epam/spring/Cat.java
+++ src/epam/spring/Cat.java
@@ -1,5 +1,8 @@
 package epam.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 class Cat implements Pet {
     public Cat() {
         System.out.println("Constr cat is created");
---
+++ src/epam/spring/ConfigAnnotations.java
@@ -0,0 +1,12 @@
+package epam.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigAnnotations {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appConfig2.xml");
+        Cat cat = context.getBean("cat", Cat.class);
+        cat.say();
+        context.close();
+    }
+}
