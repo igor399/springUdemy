f80c4b8d67f6d5680e55d8f11e2396ab07319720
Igor <88189374+igor399@users.noreply.github.com>
2022-09-06 13:33:54 +0300

    some ref

--- src/epam/spring/Dog.java
+++ src/epam/spring/Dog.java
@@ -1,8 +1,18 @@
 package epam.spring;
 
 class Dog implements Pet {
+    private String name;
+
     public Dog() {
         System.out.println("Constr Dog is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
