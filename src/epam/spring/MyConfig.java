2aeba24525e58c9ab3b28b7ba6fcb144a985154f
Igor <88189374+igor399@users.noreply.github.com>
2022-09-06 19:12:23 +0300

    conf with use Java code

---
+++ src/epam/spring/MyConfig.java
@@ -0,0 +1,10 @@
+package epam.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("epam.spring")
+public class MyConfig {
+
+}
---
+++ src/epam/spring/Test6.java
@@ -0,0 +1,12 @@
+package epam.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test6 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+        context.close();
+    }
+}
