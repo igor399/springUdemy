cc79334db7d9388fd958e97a9b51a5f7575d601a
Igor <88189374+igor399@users.noreply.github.com>
2022-09-19 13:39:31 +0300

    refactor Objects in DB

---
+++ src/hibernate_test/Test4.java
@@ -0,0 +1,26 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test4 {
+    public static void main(String[] args) {
+
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory()) {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            Employee emp = session.get(Employee.class, 1);
+//            emp.setSalary(1700);
+            session.createQuery("update Employee set salary = 1000 where firstName = 'Elena'").executeUpdate();
+            session.getTransaction().commit();
+        }
+    }
+}
