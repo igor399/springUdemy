3714e5199e56df38d2681d18b8ea56b353446004
Igor <88189374+igor399@users.noreply.github.com>
2022-09-19 12:45:45 +0300

    get java-Objects from DB

--- src/hibernate_test/Test2.java
+++ src/hibernate_test/Test2.java
@@ -5,22 +5,30 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
+
         try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory()) {
 
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Max",
-                    "Losev",
-                    "Dev",
-                    3100);
+            Employee emp = new Employee(
+                    "Oleg",
+                    "Petrov",
+                    "IT",
+                    700);
             session.beginTransaction();
             session.save(emp);
+//            session.getTransaction().commit();
+
+            int meId = emp.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+            Employee employee = session.get(Employee.class, meId);
             session.getTransaction().commit();
-            System.out.println(emp);
+            System.out.println(employee);
         }
     }
 }
