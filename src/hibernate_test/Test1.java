d5d90348a53df54f003e4b945317560febd93cbc
Igor <88189374+igor399@users.noreply.github.com>
2022-09-19 12:01:45 +0300

    save java-Objects in DB

---
+++ src/hibernate_test/Test1.java
@@ -0,0 +1,24 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Petr", "Zaicev", "Sobaka", 300);
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+    }
+}
