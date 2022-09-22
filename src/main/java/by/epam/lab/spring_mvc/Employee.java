903160864b375864557de00bf8f02919238a9978
Igor <88189374+igor399@users.noreply.github.com>
2022-09-22 18:59:17 +0300

    spring MVC form select Map implementation

--- src/main/java/by/epam/lab/spring_mvc/Employee.java
+++ src/main/java/by/epam/lab/spring_mvc/Employee.java
@@ -1,12 +1,20 @@
 package by.epam.lab.spring_mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Employee {
     private String name;
     private String surname;
     private int salary;
     private String department;
+    private Map<String, String> departments;
 
     public Employee() {
+        departments = new HashMap<>();
+        departments.put("IT", "Information Technology");
+        departments.put("HR", "Human Resources");
+        departments.put("Sales", "Sales");
     }
 
     public Employee(String name, String surname, int salary, String department) {
@@ -56,5 +64,13 @@ public class Employee {
                 ", salary=" + salary +
                 ", department='" + department + '\'' +
                 '}';
+    }
+
+    public Map<String, String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Map<String, String> departments) {
+        this.departments = departments;
     }
 }
