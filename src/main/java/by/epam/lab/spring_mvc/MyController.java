60ab3b3e15b40ff9b6acb7a0fa0063e14c16aec1
Igor <88189374+igor399@users.noreply.github.com>
2022-09-22 12:40:01 +0300

    model component

--- src/main/java/by/epam/lab/spring_mvc/MyController.java
+++ src/main/java/by/epam/lab/spring_mvc/MyController.java
@@ -1,25 +1,36 @@
 package by.epam.lab.spring_mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class MyController {
 
     @RequestMapping("/")
-    public String show(){
+    public String show() {
         return "firstView";
     }
 
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails(){
+    public String askEmployeeDetails() {
         return "ask-emp-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(){
+    public String showEmpDetails(HttpServletRequest request, Model model) {
+        String empName = request.getParameter("employeeName");
+        empName = "Mr." + empName;
+        model.addAttribute("nameAttribute", empName);
+        model.addAttribute("description", " - king of Java");
         return "show-emp-view";
     }
-
 }
--- src/main/webapp/WEB-INF/view/show-emp-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-view.jsp
@@ -5,8 +5,12 @@
 <h2>Emp welcome!</h2>
 <br>
 <br>
+<%--<h2> Your name: ${param.employeeName} </h2>--%>
 
-<h2> Your name: ${param.employeeName} </h2>
+Your name: ${nameAttribute} ${description}
+
+
+
 </body>
 
 </html>
