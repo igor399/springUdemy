6fca6d051e81a19c37126230c1637c6aadce4805
Igor <88189374+igor399@users.noreply.github.com>
2022-09-23 14:11:48 +0300

    display employees from DB data

---
+++ src/main/java/by/epam/spring/mvc_hibernate/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package by.epam.spring.mvc_hibernate.dao;
+
+import by.epam.spring.mvc_hibernate.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
---
+++ src/main/java/by/epam/spring/mvc_hibernate/dao/EmployeeDAOImpl.java
@@ -0,0 +1,27 @@
+package by.epam.spring.mvc_hibernate.dao;
+
+import by.epam.spring.mvc_hibernate.entity.Employee;
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Employee> allEmployees = session.createQuery
+                ("from Employee", Employee.class).getResultList();
+        return allEmployees;
+    }
+}
