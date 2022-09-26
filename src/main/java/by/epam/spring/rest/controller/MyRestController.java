dda7c32d1a87b7ae11051e37db29c2df9d6dc2a0
Igor <88189374+igor399@users.noreply.github.com>
2022-09-26 17:29:14 +0300

    change existing employee using JSON

--- src/main/java/by/epam/spring/rest/controller/MyRestController.java
+++ src/main/java/by/epam/spring/rest/controller/MyRestController.java
@@ -33,7 +33,13 @@ public class MyRestController {
     }
 
     @PostMapping("/employees")
-    public Employee addNewEmployee(@RequestBody Employee employee){
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
         return employee;
     }
