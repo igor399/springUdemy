ee2ffddca87f8ef5eb9259db9caf41e3e492b8b7
Igor <88189374+igor399@users.noreply.github.com>
2022-09-26 17:10:08 +0300

    global exception handling

--- src/main/java/by/epam/spring/rest/controller/MyRestController.java
+++ src/main/java/by/epam/spring/rest/controller/MyRestController.java
@@ -1,12 +1,10 @@
 package by.epam.spring.rest.controller;
 
 import by.epam.spring.rest.entity.Employee;
-import by.epam.spring.rest.exceptionHandling.EmployeeIncorrectData;
 import by.epam.spring.rest.exceptionHandling.NoSuchEmployeeException;
 import by.epam.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,19 +30,5 @@ public class MyRestController {
             throw new NoSuchEmployeeException("There is no Employee with id = " + id + " in DB");
         }
         return employee;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handlerException(NoSuchEmployeeException exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handlerException(Exception exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
