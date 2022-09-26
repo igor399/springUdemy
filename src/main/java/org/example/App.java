060b293d39a0995abae038ccb4616b3c5634a353
Igor <88189374+igor399@users.noreply.github.com>
2022-09-26 18:29:46 +0300

    getting Employee List

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.example.configuration.MyConfig;
+import org.example.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getllEmployees();
+        System.out.println(allEmployees);
     }
 }
---
+++ src/main/java/org/example/Communication.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.example.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class Communication {
+    private final String URL = "http://localhost:8080/springUdemy/api/employees";
+    @Autowired
+    private RestTemplate restTemplate;
+    public List<Employee> getllEmployees(){
+        ResponseEntity<List<Employee>> responseEntity = restTemplate.exchange(
+                URL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Employee>>() {
+        });
+        List<Employee> allEmployees = responseEntity.getBody();
+        return allEmployees;
+    }
+
+    public Employee getEmployee(int id){
+        Employee employee = restTemplate.getForObject(URL + "/" + id,
+                Employee.class);
+        return employee;
+    }
+
+    public void saveEmployee(Employee employee){
+
+    }
+
+    public void deleteEmployee(int id){
+
+    }
+}
