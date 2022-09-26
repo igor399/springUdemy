830c09c7cb3cf4881e9332b535abd09633e1590a
Igor <88189374+igor399@users.noreply.github.com>
2022-09-26 15:56:51 +0300

    get all employees using json

---
+++ src/main/java/by/epam/spring/rest/controller/MyRestController.java
@@ -0,0 +1,26 @@
+package by.epam.spring.rest.controller;
+
+import by.epam.spring.rest.entity.Employee;
+import by.epam.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+
+
+}
