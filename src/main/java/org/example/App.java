399a585235ade902cb2386b48cea8926836b91d3
Igor <88189374+igor399@users.noreply.github.com>
2022-09-26 18:31:04 +0300

    get Employee by id

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,13 +6,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.List;
 
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         Communication communication = context.getBean("communication", Communication.class);
-        List<Employee> allEmployees = communication.getllEmployees();
-        System.out.println(allEmployees);
+//        List<Employee> allEmployees = communication.getllEmployees();
+//        System.out.println(allEmployees);
+
+        Employee empById = communication.getEmployee(7);
+        System.out.println(empById);
+
+
     }
 }
