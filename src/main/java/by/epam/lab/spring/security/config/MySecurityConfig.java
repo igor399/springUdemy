be1c4188d2a82eb0ad9089f4a08f1665c2905d92
Igor <88189374+igor399@users.noreply.github.com>
2022-09-27 16:19:05 +0300

    app configuration

---
+++ src/main/java/by/epam/lab/spring/security/config/MySecurityConfig.java
@@ -0,0 +1,33 @@
+package by.epam.lab.spring.security.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+
+                .withUser(userBuilder
+                        .username("doubleDamage")
+                        .password("doubleDamage")
+                        .roles("EMPLOYEE"))
+
+                .withUser(userBuilder
+                        .username("igor")
+                        .password("igor")
+                        .roles("HR"))
+
+                .withUser(userBuilder
+                        .username("user")
+                        .password("user")
+                        .roles("MANGER", "HR"));
+    }
+}
---
+++ src/main/java/by/epam/lab/spring/security/config/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package by.epam.lab.spring.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
