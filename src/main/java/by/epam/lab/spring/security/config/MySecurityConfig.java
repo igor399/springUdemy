e1c779610f8483869cc1ef3be6bc0896a820d268
Igor <88189374+igor399@users.noreply.github.com>
2022-09-27 21:56:28 +0300

    saving encrypted password in DB using bcrypt algorithm

--- src/main/java/by/epam/lab/spring/security/config/MySecurityConfig.java
+++ src/main/java/by/epam/lab/spring/security/config/MySecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 import javax.sql.DataSource;
 
@@ -19,23 +17,6 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource);
-
-//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-//        auth.inMemoryAuthentication()
-//                .withUser(userBuilder
-//                        .username("doubleDamage")
-//                        .password("doubleDamage")
-//                        .roles("EMPLOYEE"))
-//
-//                .withUser(userBuilder
-//                        .username("igor")
-//                        .password("igor")
-//                        .roles("HR"))
-//
-//                .withUser(userBuilder
-//                        .username("user")
-//                        .password("user")
-//                        .roles("MANAGER", "HR"));
     }
 
     @Override
